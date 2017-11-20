4a205641cb06e9df5e675716215822efac28fd02
HuyVan <vannguyen231195@gmail.com>
2017-11-20 19:43:48 +0700

    init

---
+++ src/main/java/net/codejava/spring/dao/ContactDAO.java
@@ -0,0 +1,22 @@
+package net.codejava.spring.dao;
+
+import java.util.List;
+
+import net.codejava.spring.model.Contact;
+
+/**
+ * Defines DAO operations for the contact model.
+ *
+ */
+public interface ContactDAO {
+	
+	public void saveOrUpdate(Contact contact);
+	
+	public void delete(int contactId);
+	
+	public Contact get(int contactId);
+	
+	public List<Contact> list();
+	
+	public int doLogin(String usernam, String password);
+}
